204a9bb184e3c3d1c3fa69e7d176573e34d98561
Norah <norahalareefi@outlook.com>
2026-05-03 01:08:16 +0300

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by alarifi family on 16/11/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
