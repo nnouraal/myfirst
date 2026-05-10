d6478658b4664bd69fc5c02c093e2764fb2b396e
Norah Alareefi <204707215+nnouraal@users.noreply.github.com>
2026-05-10 20:53:23 +0300

     myfirstxcode

--- test/ContentView.swift
+++ test/ContentView.swift
@@ -9,16 +9,66 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        ZStack{
+            Color(.gray)
+                .ignoresSafeArea()
+            VStack(alignment: .leading, spacing: 20) {
+                
+                Image("Saudi_Arabia")
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(RoundedRectangle(cornerRadius: 16, style: .continuous))
+                    .shadow(radius: 4)
+                
+                HStack{
+                    Text("I am saudia")
+                        .font(.title)
+                        .bold()
+                    
+                    Spacer()
+                    
+                    VStack{
+                        
+                        HStack{
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                        }
+                            Text("(reviews 1M)")
+                        }
+                    .foregroundStyle(.orange)
+                    .font(.caption)
+                }
+                
+                Text("PROUD TO BE!!")
+                
+                HStack{
+                    Spacer()
+                    Image(systemName: "heart.fill")
+                    Image(systemName: "shield.fill")
+                }
+                .foregroundStyle(.gray)
+                .font(.caption)
+            }
+            .padding()
+            .background(){
+                Rectangle()
+                    .foregroundStyle(.white)
+                    .clipShape(RoundedRectangle(cornerRadius: 16, style: .continuous))
+                    .shadow(radius: 15)
+            }
+            .padding()
         }
-        .padding()
+        
+      
     }
 }
 
 #Preview {
     ContentView()
 }
+//.background(Color.yellow.opacity(0.1))
+//.cornerRadius(16)
